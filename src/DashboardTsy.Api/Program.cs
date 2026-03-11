842fa71bcc832cdee3f120ca97eb42daa48eac01
bugrahanakbas <bugrahan.akbas@mobven.com>
2026-03-11 17:12:31 +0300

    Reapply "Add Owl Carousel 2.3.4 with all assets and documentation"
    This reverts commit 6b96d66aa13b9a2663d3e321def32115a4aa5b55.

--- src/DashboardTsy.Api/Program.cs
+++ src/DashboardTsy.Api/Program.cs
@@ -1,9 +1,26 @@
+using System.Globalization;
+using DashboardTsy.Api.Data;
+using DashboardTsy.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var cultureInfo = new CultureInfo("tr-TR")
+{
+    NumberFormat = { NumberDecimalSeparator = ".", NumberGroupSeparator = "," },
+    DateTimeFormat = { ShortDatePattern = "dd.MM.yyyy" }
+};
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+var mainConn = builder.Configuration["DbConnectionStrings:Main"] ?? builder.Configuration.GetConnectionString("Main");
+var referansConn = builder.Configuration["DbConnectionStrings:Referans"] ?? builder.Configuration.GetConnectionString("Referans");
+
+builder.Services.AddDbContext<DashboardTsyDbContext>(o => o.UseSqlServer(mainConn));
+builder.Services.AddScoped<IWindowsAuthService, WindowsAuthService>();
+builder.Services.AddSingleton(new ReferansDbOptions { ConnectionString = referansConn });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
