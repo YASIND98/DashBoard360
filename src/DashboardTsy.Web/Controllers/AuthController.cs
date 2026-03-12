49f7b90d8424c8736775547348bb0f767d6cff98
bugrahanakbas <bugrahan.akbas@mobven.com>
2026-03-12 16:27:28 +0300

    Remove authorization requirement from AuthController
    The [Authorize] attribute on AuthController was commented out,
    allowing unauthenticated access to its actions. This change
    makes all controller endpoints accessible without requiring
    user authentication.

--- src/DashboardTsy.Web/Controllers/AuthController.cs
+++ src/DashboardTsy.Web/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DashboardTsy.Web.Controllers;
 
-[Authorize]
+//[Authorize]
 public class AuthController : Controller
 {
     private readonly IHttpClientFactory _httpClientFactory;
