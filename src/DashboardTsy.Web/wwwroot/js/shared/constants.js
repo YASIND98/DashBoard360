655791ae868275760eaba04d53fcf6c628cab3db
YASINDI <YASINDI@HRZDATASCI016>
2026-07-02 06:37:21 +0300

    SERVICEBUS_TOKEN_URL UPDATE (http://esb.deniz.denizbank.com/ServiceBus)

--- src/DashboardTsy.Web/wwwroot/js/shared/constants.js
+++ src/DashboardTsy.Web/wwwroot/js/shared/constants.js
@@ -31,7 +31,7 @@ var TOP10_PRODUCT_NAMES = [
 var SCORE_CARD_BASE_URL = "https://api-pupa.apps.prod.deniz.denizbank.com/esbnode.asmx/api";
 
 // ServiceBus OAuth (client_credentials) -> skor kart servis çağrılarına eklenecek Bearer token
-var SERVICEBUS_TOKEN_URL = "http://esb.deniz.denizbank.com/ServiceBus/oauth/token";
+var SERVICEBUS_TOKEN_URL = "http://esb.deniz.denizbank.com/ServiceBus";
 var SERVICEBUS_CLIENT_ID = "YonetimRaporToPupa";
 var SERVICEBUS_CLIENT_SECRET = "19ea09f7714271b98b79332c36b16908262ea5ddc9b8fdb8a687981f1318c97f";
 // audience = skor kart servisinin origin'i; SCORE_CARD_BASE_URL'den türetilir (tek kaynak)
