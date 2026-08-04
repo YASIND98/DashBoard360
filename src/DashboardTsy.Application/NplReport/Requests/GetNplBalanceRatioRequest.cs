438b48032a887632d2aadd942150c1b863e9ca9d
bugrahanakbas <bugrahan.akbas@mobven.com>
2026-08-04 15:50:44 +0300

    fix

--- src/DashboardTsy.Application/NplReport/Requests/GetNplBalanceRatioRequest.cs
+++ src/DashboardTsy.Application/NplReport/Requests/GetNplBalanceRatioRequest.cs
@@ -14,4 +14,16 @@ public class GetNplBalanceRatioRequest
     public string? Urun { get; set; }
     public int? Yil { get; set; }
     public string? YetkiKodu { get; set; }
+
+    public bool? BonusBusinessFlag { get; set; }
+    public bool? BireyselMikroFlag { get; set; }
+    public int? IrsFlag { get; set; }
+    public bool? YapilandirmaFlag { get; set; }
+    public string? YapilandirmaFlagKredi { get; set; }
+    public bool? IhtiyacTicariFlag { get; set; }
+    public bool? KgfliKrediFlag { get; set; }
+    public bool? KgfliMustFlag { get; set; }
+    public bool? EmekliFlag { get; set; }
+    public bool? DbMaasOdemesiFlag { get; set; }
+    public string? Ob { get; set; }
 }
