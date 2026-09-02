3f74f153c78cc36b615a81be6c6f2f4b2da181ed
bugrahanakbas <bugrahan.akbas@mobven.com>
2026-09-02 14:38:37 +0300

    swagger fix

--- src/DashboardTsy.Api/Program.cs
+++ src/DashboardTsy.Api/Program.cs
@@ -159,12 +159,12 @@ builder.Services.Configure<GzipCompressionProviderOptions>(o => o.Level = Compre
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
